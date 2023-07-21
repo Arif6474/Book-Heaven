283987f00c2615b1d7d50871fafcf318fdaddcae
Arif Islam <arifulislam64743@gmail.com>
2023-07-21 23:14:24 +0600

    filter book

--- src/components/AllBooks/AllBooks.tsx
+++ src/components/AllBooks/AllBooks.tsx
@@ -29,8 +29,7 @@ function AllBooks() {
     dispatch(setSearchTerm(searchQuery));
   }, [dispatch, searchQuery]);
 
-  
-  const filteredData = books?.filter(
+  const searchAndFilterBook = books?.filter(
     (item: { title: string; author: string; genre: string }) => {
       const itemValues = Object.values(item).map((value) =>
         value.toString().toLowerCase()
@@ -41,21 +40,72 @@ function AllBooks() {
     }
   );
 
+  //filter
+  const [filterByGenre, setFilterByGenre] = useState("");
+  const [filterByYear, setFilterByYear] = useState("");
+
+   const handleFilterByGenre = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterByGenre(e.target.value);
+  };
+
+  const handleFilterByYear = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterByYear(e.target.value);
+  };
+
+  const filteredBooks = searchAndFilterBook?.filter(
+    (book: { genre: string; publication_date: string | string[] }) => {
+      const genreMatch =
+        filterByGenre === "" ||
+        book.genre.toLowerCase().includes(filterByGenre.toLowerCase());
+      const yearMatch =
+        filterByYear === "" || book.publication_date.includes(filterByYear);
+      return genreMatch && yearMatch;
+    }
+  );
   return (
     <div className="p-10">
       <div className="form-control px-10 flex flex-row justify-between">
         <input
           type="text"
           placeholder="Search"
-          className="input input-bordered w-24 md:w-auto"
+          className="input input-bordered border-accent w-24 md:w-auto"
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <Link to='/addNewBook'>
-          <button className="btn btn-success text-slate-900">Add New Book</button>
+        <Link to="/addNewBook">
+          <button className="btn btn-success text-slate-900">
+            Add New Book
+          </button>
         </Link>
       </div>
+      <div className=" pt-4 flex gap-4 mx-10">
+        
+        <select
+          value={filterByGenre}
+          onChange={handleFilterByGenre}
+          className="select select-accent w-[200px] max-w-xs "
+        >
+          <option value="">Filter by Genre</option>
+           {
+            books?.map((book: Book) =>
+              <option key={book._id} value={book.genre}>{book.genre}</option>
+              )
+           }
+        </select>
+        <select
+         value={filterByYear}
+         onChange={handleFilterByYear}
+          className="select select-accent  w-[200px] max-w-xs"
+        >
+          <option value="">Filter by Publication Year</option>
+           {
+            books?.map((book: Book) =>
+              <option key={book._id} value={book.publication_date}>{book.publication_date}</option>
+              )
+           }
+        </select>
+      </div>
       <div className="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-20 p-10 mt-10">
-        {filteredData?.map((book: Book) => (
+        {filteredBooks?.map((book: Book) => (
           <SingleBook key={book._id} book={book} />
         ))}
       </div>
