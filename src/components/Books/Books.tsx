cf0054c7147c7f35c0fc560a07f113be205a9f87
Arif Islam <arifulislam64743@gmail.com>
2023-07-21 20:50:52 +0600

    update

--- src/components/Books/Books.tsx
+++ src/components/Books/Books.tsx
@@ -1,11 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useGetBooksQuery } from "../../redux/features/books/bookAPIs";
-import { IBook } from "../../types/globalTypes";
 import SingleBook from "../AllBooks/SingleBook/SingleBook";
-
+interface Book {
+  _id: string;
+  title: string;
+  img: string;
+  author: string;
+  genre: string;
+  publication_date: string;
+  reviews: any[];
+}
 function Books() {
   const { data: books } = useGetBooksQuery(undefined);
   // console.log("🚀 ~ file: Books.tsx:10 ~ Books ~ books:", books)
@@ -15,7 +23,7 @@ function Books() {
       <h1 className="text-center text-xl font-bold pt-10">Latest Books</h1>
 
       <div className="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-20 p-8 ">
-        {reversedBooksArr?.slice(0,10)?.map((book: IBook) => (
+        {reversedBooksArr?.slice(0,10)?.map((book: Book) => (
           <SingleBook key={book._id} book={book} />
         ))}
       </div>
