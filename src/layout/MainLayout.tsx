415e183db92ae1cf7dbc9f4be0ab1c236d1d7d2e
Arif Islam <arifulislam64743@gmail.com>
2023-07-21 22:02:12 +0600

    added footer

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
 
 
 export default function MainLayout() {
@@ -9,6 +10,7 @@ export default function MainLayout() {
       <div >
         <Outlet />
       </div>
+      <Footer/>
     </div>
   );
 }
